b45f05e47d6c27a8ee51cc5e956324653b1bd444
Irina Brennen <irinabrennen@twc.com>
2017-09-01 18:23:11 -0500

    added logic to display weather description based on users coordinates

--- js/index.js
+++ js/index.js
@@ -1,5 +1,39 @@
-$.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=43.1242711&lon=87.9105899",function(data){
-	$("#clouds").html(data.weather[0].description);
+var latitude ;
+var longitude;
 
+var url = "https://fcc-weather-api.glitch.me/api/current";
+
+var getWeather = function(my_coords) {
+		$.getJSON(url, my_coords, function(data) {
+				$("#weather-description").html(data.weather[0].description);
+		});
+
+};
+var getPosSuccess = function(pos){
+
+	latitude = pos.coords.latitude ;
+	longitude = pos.coords.longitude ;
+	var myCoords = { lon : longitude, lat : latitude };
+	getWeather(myCoords);	
 	
-});
+};
+
+var getPosError = function(err){
+
+	throw err;
+	
+};
+
+
+
+
+
+var main = function(){
+
+  
+	navigator.geolocation.getCurrentPosition(getPosSuccess, getPosError);
+		
+};
+
+$(document).ready(main);
+
